51e18f558364854cf04d3a4f683650051f7daad0
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-06-03 16:57:41 +0530

    mongodbIntro

---
+++ I_Mongo.js
@@ -0,0 +1,39 @@
+// Data Storage 
+// aesa kuch jh ap hm sare users  ki info store krte h
+// file - data.txt but only text, not img, audio, videos big files => DATABASE
+
+// Types of DBS
+// SQL, NoSQL
+// SQL - data ko ek roop m rkhna i.e. tables
+// actual m tables ni hote, pr iss way m smjhna asan hota h, data is scattered actually, but smjh ni aata aese
+
+// NoSQL - objects way m data hota h
+// MongoDB is database which NoSQL theory
+// NoSQL - concept
+// MongoDB - database
+// {
+//     age: 25
+//     name: "hey"
+// }
+
+// what and why
+// why mongodb : to store data
+
+// terminologies - collections, schemas, keys, models
+// 2 servers in backend - application server and database server
+// Node is application server
+// MongoDB is database server
+
+// application server - handles routes, req ko accept krta h, sb kuch krta h except data related work
+// larger entity - database
+// smaller entity inside database - collections
+// one person in collections - document
+
+// Database -> Collections -> Document
+
+// CODE                DATABASE
+// mongoose.connect -> database created
+// model create     -> collection created
+// CREATE           -> document created
+
+// mongodb compass install
